c880dbd191bc86452774fa45da0e6c8b3e862a68
pdhruva500 <pdhruva@eastsideprep.org>
2025-11-09 09:23:42 -0800

    commit for prototype

---
+++ js/forms.js
@@ -0,0 +1,8 @@
+    
+function submitForm() {
+    let fname = document.getElementById("fname").value;
+    alert("Thank you, " + fname + "! Your lesson request has been submitted. We'll be in touch soon.");
+}
+
+
+
